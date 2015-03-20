51db0f7a1ec7ef9beb48ff13c3621287e2931318
badman <365623454@qq.com>
2015-03-20 15:21:41 +0800

    add file myobject.py

---
+++ python_test/myobject.py
@@ -0,0 +1,24 @@
+class Student(object):
+    
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s: %s', self.__name, self.__score)
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+
