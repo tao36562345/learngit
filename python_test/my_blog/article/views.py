05e1b6b88385601bbca09e85b0d2f9c696e744c2
badman <dengtao@bad.local>
2015-04-16 22:35:32 +0800

    update article

--- python_test/my_blog/article/views.py
+++ python_test/my_blog/article/views.py
@@ -19,3 +19,11 @@ def detail(request, id):
 
 def test(request):
     return render(request, 'test.html', {'current_time': datetime.now()})
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list,
+                                             'error' : False})
